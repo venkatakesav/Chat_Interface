930a85765a86fc09293f34478276dfa34126e4ab
Venkata Kesav Venna <venkatakesavvenna@Lavanya.local>
2024-06-18 10:20:38 +0530

    Added Database Commit

--- app.py
+++ app.py
@@ -4,32 +4,41 @@ import google.generativeai as genai
 from PIL import Image
 import os
 import io
+import json
 
 api_key = "AIzaSyBtMmUSa0eOLjCKpf7yDlNHw-g92W-5d5c"
 
-def sepia(text, input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189], 
-        [0.349, 0.686, 0.168], 
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return "hello World"
+with open("database.json", "r") as fp:
+    data = json.load(fp)
 
 def gemini_model(text, input_img):
+    input_img = Image.open(input_img)
     genai.configure(api_key=api_key)
-
-    img = Image.fromarray(input_img)
-
     model = genai.GenerativeModel(model_name="gemini-1.5-pro")
-    response = model.generate_content([text, img])
+    response = model.generate_content([text, input_img])
     return response.text
 
-demo = gr.Interface(
-    fn=gemini_model,
-    inputs=["text", gr.Image()],
-    outputs=["text"],
+def echo(message, history):
+    file_path = message["files"][0]
+    output = ""
+    for entry in data:
+        if file_path.split("/")[-1] == entry["image"].split('/')[-1]:
+            output = entry["conversation"][0]["a"]
+    print("Message", message)
+    return output
+
+demo = gr.ChatInterface(
+    fn=echo,
+    examples=[{"text": "Describe the Image:", "files": ["images/bahubali.png"]}, \
+              {"text": "Biryani", "files": ["images/biryani.png"]}, \
+              {"text": "Kali", "files": ["images/kali.png"]}, \
+              {"text": "Narasimha", "files": ["images/narasimha.png"]}, \
+              {"text": "Salman Khan", "files": ["images/Salkhan.png"]}, \
+              {"text": "Siva", "files": ["images/siva.png"]}, \
+              {"text": "Statue Of Unity", "files": ["images/statueunity.png"]}, \
+              ],
+    title="Chat Interface Demo",
+    multimodal=True,
 )
 
 if __name__ == "__main__":
